28fa1e93c82801a8093c263d9082453554ed0807
MS20 WSChang0 <WSChang0@nuvoton.com>
2019-08-06 10:33:45 +0800

    Create

---
+++ SampleCode/Semihost/main.c
@@ -0,0 +1,59 @@
+/****************************************************************************
+ * @file     main.c
+ * @version  V3.00
+ * $Revision: 7 $
+ * $Date: 15/11/03 9:27a $
+ * @brief    Show how to debug with semi-host message print.
+ * @note
+ * Copyright (C) 2014 Nuvoton Technology Corp. All rights reserved.
+ *
+ *****************************************************************************/
+
+#include <stdio.h>
+#include "NUC1311.h"
+
+#if defined (__GNUC__)
+extern void initialise_monitor_handles(void);
+#endif
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Main Function                                                                                            */
+/*---------------------------------------------------------------------------------------------------------*/
+
+int32_t main()
+{
+    int8_t item;
+
+#if (defined (__GNUC__) && (!(defined(__ARMCC_VERSION))))
+    initialise_monitor_handles();
+#endif
+
+    /*
+        To enable semihost, user must define "DEBUG_ENABLE_SEMIHOST" constant when build code with NUC1311 BSP.
+        This sample code is used to show how to print message/getchar on IDE debug environment.
+        It will echo all input character back on UART #1 of KEIL IDE.
+
+        In KEIL MDK, user need to open "View->Serial Window->UART #1" windows in debug mode.
+        In IAR Workbench, user need to open "View->Terminal I/O" in debug mode.
+
+        NOTE1: HardFault_Handler handler is implemented in retarget.c.
+        NOTE2: Semihost only works with Nuvoton NuLink ICE Dongle in debug mode.
+        NOTE3: It does not print any message if Nuvoton NuLink ICE Dongle is not connected.
+    */
+
+    printf("\n Start SEMIHOST test: \n");
+
+    while(1)
+    {
+        /* Get input character */
+        item = getchar();
+
+        /* Print input character back */
+        printf("%c\n", item);
+    }
+}
+
+/*** (C) COPYRIGHT 2014 Nuvoton Technology Corp. ***/
+
+
+
